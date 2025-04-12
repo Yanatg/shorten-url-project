cb7a11310fb9eae174ded02f4c8c733df6927123
Yanatchara JERAJA <yanatchara.j@ku.th>
2025-04-12 14:47:23 +0700

    remove /css/app.css

--- app/Views/login_form.php
+++ app/Views/login_form.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="/css/app.css">
     <link rel="stylesheet" href="<?= base_url('css/style.css') ?>">
 </head>
 
--- app/Views/register_form.php
+++ app/Views/register_form.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register New Account</title>
-    <link rel="stylesheet" href="/css/app.css">
     <link rel="stylesheet" href="<?= base_url('css/style.css') ?>">
 </head>
 
